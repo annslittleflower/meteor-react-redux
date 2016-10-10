12f840de2edcfbd30d4e804c70148659e1d65a8c
sasha.bashliy <sashkacrab@gmail.com>
2016-10-10 21:05:13 +0300

    last todays commit

--- imports/ui/layouts/MainLayout.js
+++ imports/ui/layouts/MainLayout.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
 const MainLayout = ({ content }) => {
   return <div>
